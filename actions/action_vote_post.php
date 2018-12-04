14d05acc9da08b88943b3084305126769a343387
Duarte-Frazao <duartemarquesmano@gmail.com>
2018-12-04 16:12:19 +0000

    Added login redirection when voting not logged in

--- actions/action_vote_post.php
+++ actions/action_vote_post.php
@@ -2,14 +2,21 @@
     include_once("../includes/session.php");
     include_once("../database/db_vote.php");
 
-    if(!isset($_SESSION['id']))
-        header("Location: ../pages/login.php");
+    $permission = true;
+    $entities = array();
 
-    $type = $_POST['voteType'];
-    $entityID = $_POST['entityID'];
+    if(!isset($_SESSION['username'])){
+        $permission = false;
+    }else{
+        $type = $_POST['voteType'];
+        $entityID = $_POST['entityID'];
+        
+        vote($entityID,$type,$_SESSION['id']);
+        $entities = getVoteUser($entityID,$_SESSION['id']);
+    }
+
+    $response = array('result' => $permission, 'data' => $entities);
+
+    echo json_encode($response);
     
-    vote($entityID,$type,$_SESSION['id']);
-    $info = getVoteUser($entityID,$_SESSION['id']);
-    
-    echo json_encode($info); 
 ?>
